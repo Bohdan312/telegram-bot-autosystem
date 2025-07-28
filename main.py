cdb0720b5a6f31d7e995ef2d71bb789513094d9f
Bohdan312 <129343223+Bohdan312@users.noreply.github.com>
2025-07-28 20:05:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 🔐 1. Розкодовуємо облікові дані Google Sheets з base64
-creds_b64 = os.getenv("GOOGLE_CREDENTIALS_JSON_BASE64")
+creds_b64 = os.getenv("	
+df2340767251dd9bed9a8783731ce0ad5d45191a")
 if not creds_b64:
     raise Exception("BOT_TOKEN_B64=ODAyNjI5Njg4NTpBQUZDbUQ5SmZQbWlSUXpGc2czTnU4dkxETkZQV1ljdFk1OA==
 ")
